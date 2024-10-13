a1a77dbe7e3722d9a441d422fa60f53c4e84938d
sadia-jannat59 <sadia68959@gmail.com>
2024-10-13 23:58:55 +0600

    test

--- Script.js
+++ Script.js
@@ -1,29 +1,50 @@
  
+ $(function() {
+  // Owl Carousel
+  var owl = $(".owl-carousel");
+  owl.owlCarousel({
+    margin: 10,
+    loop: true,
+    nav: true,
+    responsive:{
+      0:{
+        items:1,
+      },
+      400:{
+        items:1,
+      },
+      1000:{
+        items:4,
+      },
+    }
+  });
+});
+
 
  
-  const s=window.innerWidth;
-  if(s<=640){
-    $(function() {
-      // Owl Carousel
-      var owl = $(".owl-carousel");
-      owl.owlCarousel({
-        items:1,
-        margin: 10,
-        loop: true,
-        nav: true,
-        autoplay:true
-      });
-    });
-  }else{
-    $(function() {
-      // Owl Carousel
-      var owl = $(".owl-carousel");
-      owl.owlCarousel({
-        items: 4,
-        margin: 10,
-        loop: true,
-        nav: true,
-        autoplay:true
-      });
-    });
-  }
+  // const s=window.innerWidth;
+  // if(s<=640){
+  //   $(function() {
+  //     // Owl Carousel
+  //     var owl = $(".owl-carousel");
+  //     owl.owlCarousel({
+  //       items:1,
+  //       margin: 10,
+  //       loop: true,
+  //       nav: true,
+  //       autoplay:true
+  //     });
+  //   });
+  // }else{
+  //   $(function() {
+  //     // Owl Carousel
+  //     var owl = $(".owl-carousel");
+  //     owl.owlCarousel({
+  //       items: 4,
+  //       margin: 10,
+  //       loop: true,
+  //       nav: true,
+  //       autoplay:true
+  //     });
+  //   });
+  // }
